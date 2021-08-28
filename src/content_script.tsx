a754fd6e294b1ce4140422b3e4ff0ff8687a354f
Jason Chen <42862982+ZhiruiJC@users.noreply.github.com>
2021-08-28 20:05:49 +1000

    merged with filter

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,39 @@
+const array = ['Paris', 'France', 'Europe', 'CSS'];
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.color) {
     console.log("Receive color = " + msg.color);
     document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
+    sendResponse("Change colorr to " + msg.color);
+  } 
+  else if (msg.scrape) {
+
+      var elements = document.getElementsByTagName('*');
+
+      for (var i = 0; i < elements.length; i++) {
+          var element = elements[i];
+
+          for (var j = 0; j < element.childNodes.length; j++) {
+              var node = element.childNodes[j];
+
+              if (node.nodeType === 3) {
+                  for (let i = 0; i < array.length; i++) {
+                      var text = node.nodeValue;
+
+                      if (text != null) {
+
+                          node.nodeValue = text.replace(new RegExp(array[i], 'gi'), "temp");
+
+                      }
+                  }
+              }
+          }
+      }
+      document.body.innerHTML = document.body.innerHTML.replace(new RegExp("temp", 'gi'),
+          '<span style="background-color: black; color: black;">temp</span>');
+      console.log("nice")
+  }
+  else {
     sendResponse("Color message is none.");
   }
 });
---
+++ src/filter/scrape.tsx
@@ -0,0 +1,43 @@
+const badWords = [
+  "abbo",
+  "abo",
+  "abortion",
+  "abuse",
+  "addict",
+  "addicts",
+  "adult",
+  "africa",
+  "african",
+  "alla",
+  "allah",
+  "sht",
+  "shit",
+  "fuck"
+]
+
+export default function scrapePage () {
+  console.log("hello we will scrape the pageddd")
+  // blur levels
+  const blur_levels = ["0px", "4px", "20px"];
+  const text_node_id = 3
+  const element_node_id = 1;
+  
+  chrome.tabs.query({currentWindow: true, active: true},
+  function(tabs) {
+    const tab = tabs[0];
+    console.log(tab)
+    if (tab.id) {
+      chrome.tabs.sendMessage(tab.id,
+      {
+        scrape: true
+      }, function(response) {
+        // response?
+        console.log("hasdfa")
+        let textBody = response.textBody
+      });
+    }
+  });
+
+}
+
+// 
