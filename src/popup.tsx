f198914a07576888b1d64dba4f45419465fcf6db
Heidi Chan <z5024971@student.unsw.edu.au>
2021-08-28 19:47:45 +1000

    master branch merge

--- src/popup.tsx
+++ src/popup.tsx
@@ -7,20 +7,10 @@ import CameraIcon from '@atlaskit/icon/glyph/camera';
 import WatchIcon from '@atlaskit/icon/glyph/watch';
 import Snapshot from "./snapshot";
 
-import "@atlaskit/css-reset";
-import { N0, N100 } from "@atlaskit/theme/colors";
-import { e100 } from "@atlaskit/theme/elevation";
-import { borderRadius, gridSize } from "@atlaskit/theme/constants";
 
 const Popup = () => {
   const [currentURL, setCurrentURL] = useState<string>();
   const [page, setPage] = useState(0);
-
-  useLayoutEffect(() => {
-    document.documentElement.style.height = "max-content";
-    document.body.style.background = N100;
-    document.body.style.margin = "0";
-  }, []);
 
   return (
     <>
@@ -28,12 +18,7 @@ const Popup = () => {
       @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Space+Grotesk:wght@300&display=swap');
       </style>
 
-      <div className="extension-container" style={{
-        backgroundColor: N0,
-        boxShadow: e100(),
-        borderRadius: borderRadius(),
-        padding: gridSize(),
-      }}>
+      <div className="extension-container">
         {page ? (
           <h1>Filter stuff</h1>
         ) : (
--- webpack/webpack.common.ts
+++ webpack/webpack.common.ts
@@ -12,6 +12,7 @@ const config: Configuration = {
     options: path.join(srcDir, "options.tsx"),
     background: path.join(srcDir, "background.ts"),
     content_script: path.join(srcDir, "content_script.tsx"),
+    display: path.join(srcDir, 'display.tsx'),
   },
   output: {
     path: path.join(__dirname, "../dist/js"),
