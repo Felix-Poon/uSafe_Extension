61bfa83a3776b3d3c8f457d3b6d1860c9eba7b21
Sarah Liu <sliu.ailin8801@gmail.com>
2021-08-28 18:27:03 +1000

    Download screenshots and data file

--- src/snapshot.tsx
+++ src/snapshot.tsx
@@ -3,138 +3,160 @@ import Button from "@atlaskit/button";
 import ReactDOM from "react-dom";
 import { consolidateStreamedStyles } from "styled-components";
 
-const Snapshot = () =>{
+interface Screenshot {
+  uri: string;
+  text: string[];
+}
+
+const Snapshot = () => {
   const [numScreenshots, setNumScreenshots] = useState<number>(0);
-  chrome.storage.local.get('screenshots', function(result){
+  chrome.storage.local.get('screenshots', function (result) {
     if (result.screenshots != undefined) {
       setNumScreenshots(result.screenshots.length);
     }
   });
   const [userList, setUserList] = useState<string[]>([]);
 
-  function handleChange(e:any, id:any) {
+  function handleChange(e: any, id: any) {
     let newList = [...userList];
     newList[id] = e.target.value;
     setUserList(newList);
   }
 
-    function addFormFields() {
-      setUserList([...userList, ""]);
-    }
+  function addFormFields() {
+    setUserList([...userList, ""]);
+  }
 
-    function downloadScreenshot(data : string) {
-        const link = document.createElement("a");
-        link.download = "screenshot";
-        link.href = data;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    }
+  function downloadScreenshot(data: string) {
+    const link = document.createElement("a");
+    link.download = "screenshot";
+    link.href = data;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 
-    function downloadScreenshots() {
-        chrome.storage.local.get('screenshots', function(result){
-            let screenshots: string[] = [];
-            if (result.screenshots != undefined) {
-                console.log(result.screenshots);
-                result.screenshots.map((uri:string, index: number) => console.log("index: " + index));
-            }
-            // screenshots.map(uri => downloadScreenshot(uri));
-            chrome.storage.local.clear();
-            setNumScreenshots(0);
-        });
-    }
+  function downloadScreenshots() {
+    chrome.storage.local.get('screenshots', function (result) {
+      let screenshots: string[] = [];
+      if (result.screenshots != undefined) {
+        console.log(result.screenshots);
+        result.screenshots.map((uri: string, index: number) => console.log("index: " + index));
+      }
+      // screenshots.map(uri => downloadScreenshot(uri));
+      chrome.storage.local.clear();
+      setNumScreenshots(0);
+    });
+  }
 
-    function screenShot() {
-        console.log("screenshot taken");
-        chrome.tabs.captureVisibleTab(function(data) {
-            let screenshots: string[] = [];
-            chrome.storage.local.get('screenshots', function(result){
-                console.log("get finished");
-                if (result.screenshots != undefined) {
-                    screenshots = result.screenshots;
-                }
-                console.log(screenshots);
-                screenshots.push(data);
-                chrome.storage.local.set({'screenshots': screenshots});
-                setNumScreenshots(screenshots.length);
-            });
-        });
-        downloadText();
-    }
-
-    function createTab() {
-        chrome.tabs.create({url: chrome.extension.getURL("screenshots.html")});
-    }
-
-    //https://stackoverflow.com/a/18197341
-    function downloadText() {
-      //Text to include date, url, users involved? 
-      let text_arr: string[] = [];
-
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        let url_str = tabs[0].url;
-        if (url_str) {
-          text_arr.push(url_str);
-        } else {
-          text_arr.push('url-not-found');
+  function screenShot() {
+    console.log("screenshot taken");
+    chrome.tabs.captureVisibleTab(function (data) {
+      let screenshots: Screenshot[] = [];
+      chrome.storage.local.get('screenshots', function (result) {
+        console.log("get finished");
+        if (result.screenshots != undefined) {
+          screenshots = result.screenshots;
         }
 
-        let date = new Date();
+        let text_arr: string[] = [];
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+          text_arr.push((screenshots.length).toString());
+          let url_str = tabs[0].url;
+          if (url_str) {
+            text_arr.push(url_str);
+          } else {
+            text_arr.push('url-not-found');
+          }
 
-        //https://stackoverflow.com/a/8363049 format
-        let dateStr = 
-          date.getUTCFullYear() + "-" +
-          ("0" + (date.getUTCMonth()+1)).slice(-2) + "-" + 
-          ("0" + date.getUTCDate()).slice(-2) + " " + 
-          ("0" + date.getUTCHours()).slice(-2) + ":" + 
-          ("0" + date.getUTCMinutes()).slice(-2) + ":" + 
-          ("0" + date.getUTCSeconds()).slice(-2);
-        text_arr.push(dateStr);
+          let date = new Date();
 
-        let users = userList.join(', ');
-        text_arr.push(users);
+          //https://stackoverflow.com/a/8363049 format
+          let dateStr =
+            date.getUTCFullYear() + "-" +
+            ("0" + (date.getUTCMonth() + 1)).slice(-2) + "-" +
+            ("0" + date.getUTCDate()).slice(-2) + " " +
+            ("0" + date.getUTCHours()).slice(-2) + ":" +
+            ("0" + date.getUTCMinutes()).slice(-2) + ":" +
+            ("0" + date.getUTCSeconds()).slice(-2);
 
-        let text = text_arr.join('\n');
-        var element = document.createElement('a');
-        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-        element.setAttribute('download', 'data.txt');
-      
-        element.style.display = 'none';
-        document.body.appendChild(element);
-      
-        element.click();
-      
-        document.body.removeChild(element);  
+          text_arr.push(dateStr);
+          const new_screen:Screenshot = {uri: data, text: text_arr};
+          screenshots.push(new_screen);
+          chrome.storage.local.set({ 'screenshots': screenshots});
+          setNumScreenshots(screenshots.length);
+          console.log(screenshots);
+        });
+      });
+    });
+  }
 
-     });
-    }
+  // {
+  //       'screenshots': [
+  //         {'id': 'id', 'uri': "sjdhfkjhsf", "text": "text array containing date and url"}
+  //       ]
+  // }
 
-    // return (
-    //     <>
-    //         <Button appearance="danger" onClick={screenShot}>Take a screenshot!</Button>
-    //         <Button appearance="danger" onClick={downloadScreenshots}>Download screenshots!</Button>
-    //         <Button appearance="danger" onClick={createTab}>View Screenshots</Button>
-    //         <h1> Number of screenshots taken: {numScreenshots} </h1>
-    //     </>
-    // );
+  function download() {
+    let text_arr:string[] = [];
+    let users = userList.join(', ');
+    text_arr.push(users);
 
-    return (
-        <>
-          <form onSubmit={screenShot} className="form">
-            <h5>Download evidence of online harrassment.</h5>
-            <h6>Add the names of users involved. Current date and screenshot will be downloaded automatically.</h6>
-            <div className="form-input">
-              {userList.map((e, id) => (
-                  <input type="text" onChange={e => handleChange(e, id)} />
-              ))}
-            </div>
-            <div className="form-buttons">
-              <Button appearance="primary" type="button" onClick={() => addFormFields()}>Add user</Button> <br/>
-              <Button appearance="warning" type="submit">Download</Button>
-            </div>
-          </form>
-        </>
-    );
+    chrome.storage.local.get('screenshots', function(result){
+      console.log(result);
+      if (result.screenshots != undefined) {
+        console.log(result);
+        result.screenshots.map( (s: Screenshot) => {
+          const url_data:string = s.text.join(' ');
+          console.log(s.text);
+          text_arr.push(url_data);
+          // downloadScreenshot(s.uri);
+        })
+      }
+      console.log(result);
+    });
+
+    
+    //Downloads all screenshots and user list 
+
+    let text = text_arr.join('\n');
+    console.log(text);
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', 'data.txt');
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    // element.click();
+
+    document.body.removeChild(element);
+    chrome.storage.local.clear();
+
+  }
+
+  function createTab() {
+    chrome.tabs.create({ url: chrome.extension.getURL("screenshots.html") });
+  }
+
+  return (
+    <>
+      <form onSubmit={download} className="form">
+        <h5>Download evidence of online harrassment.</h5>
+        <h6>Add the names of users involved. Current date and screenshot will be downloaded automatically.</h6>
+        <div className="form-input">
+          {userList.map((e, id) => (
+            <input type="text" onChange={e => handleChange(e, id)} />
+          ))}
+        </div>
+        <div className="form-buttons">
+          <Button appearance="primary" type="button" onClick={() => screenShot()}>Add screenshot</Button> <br />
+          <Button appearance="primary" type="button" onClick={() => addFormFields()}>Add user</Button> <br />
+          <Button appearance="warning" type="submit">Download All</Button>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default Snapshot;
