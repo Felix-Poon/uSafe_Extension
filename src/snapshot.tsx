e5388bbc6dadfbf5d5ca2f8c445fe440abe75194
Heidi Chan <z5024971@student.unsw.edu.au>
2021-08-28 14:17:22 +1000

    Downloads text file with date

--- src/snapshot.tsx
+++ src/snapshot.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import { consolidateStreamedStyles } from "styled-components";
 
 const Snapshot = () =>{
-    
+
     function downloadScreenshot(data : string) {
         const link = document.createElement("a");
         link.download = "screenshot";
@@ -18,7 +18,39 @@ const Snapshot = () =>{
         chrome.tabs.captureVisibleTab(function(data) {
             downloadScreenshot(data);
         })
+        downloadText();
     }
+    //https://stackoverflow.com/a/18197341
+    function downloadText() {
+      //Text to include date, url, users involved? 
+      let text_arr: string[] = [];
+      let date = new Date();
+
+      //https://stackoverflow.com/a/8363049 format
+      let dateStr = 
+        date.getUTCFullYear() + "-" +
+        ("0" + (date.getUTCMonth()+1)).slice(-2) + "-" + 
+        ("0" + date.getUTCDate()).slice(-2) + " " + 
+        ("0" + date.getUTCHours()).slice(-2) + ":" + 
+        ("0" + date.getUTCMinutes()).slice(-2) + ":" + 
+        ("0" + date.getUTCSeconds()).slice(-2);
+      text_arr.push(dateStr);
+
+      let text = text_arr.toString();
+      var element = document.createElement('a');
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+      element.setAttribute('download', 'data.txt');
+    
+      element.style.display = 'none';
+      document.body.appendChild(element);
+    
+      element.click();
+    
+      document.body.removeChild(element);
+    }
+    
+
+    
     return (
         <>
             <Button appearance="danger" onClick={screenShot}>Take a screenshot!</Button>
