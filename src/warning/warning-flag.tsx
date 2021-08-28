cab5a9ebb182ccba19223cd0194a4e17f8876b15
Heidi Chan <z5024971@student.unsw.edu.au>
2021-08-29 01:21:00 +1000

    flag style

--- src/warning/warning-flag.tsx
+++ src/warning/warning-flag.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 import InfoIcon from "@atlaskit/icon/glyph/info";
-import { R400 } from "@atlaskit/theme/colors";
+import { R400, N500 } from "@atlaskit/theme/colors";
 
 import Flag from "@atlaskit/flag";
+import CrossIcon from "@atlaskit/icon/glyph/cross";
 
 import {
   ExitingPersistence,
@@ -16,6 +17,8 @@ import root from "react-shadow";
 
 import { CacheProvider } from "@emotion/core";
 import createCache from "@emotion/cache";
+
+import styles from "@atlaskit/css-reset/dist/bundle.css";
 
 type WarningFlagProps = {
   type: string;
@@ -62,6 +65,7 @@ const SafetyGuideFlag = ({ type }: WarningFlagProps) => {
           right: 16,
           top: 8,
         }}
+        id="body"
       >
         {myCache && (
           <CacheProvider value={myCache}>
@@ -75,6 +79,27 @@ const SafetyGuideFlag = ({ type }: WarningFlagProps) => {
                 >
                   {(props) => (
                     <div {...props}>
+                      <style type="text/css">
+                        {styles.replace(/\bbody\b/g, "#body")}
+                        {`#body { background: none; }`}
+                      </style>
+                      <button
+                        style={{
+                          width: 24,
+                          height: 24,
+                          padding: 4,
+                          position: "absolute",
+                          top: 20,
+                          right: 16,
+                          border: 0,
+                          background: "none",
+                          color: N500,
+                          cursor: "pointer",
+                        }}
+                        onClick={hideFlag}
+                      >
+                        <CrossIcon label="" size="small" />
+                      </button>
                       <Flag
                         icon={<InfoIcon label="Info" primaryColor={R400} />}
                         id="warning"
@@ -87,6 +112,7 @@ const SafetyGuideFlag = ({ type }: WarningFlagProps) => {
                           { content: "Close", onClick: hideFlag },
                         ]}
                         onDismissed={hideFlag}
+
                       />
                     </div>
                   )}
