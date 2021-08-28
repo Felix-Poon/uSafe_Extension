4daa1f02e912ee04381584a85f5eb419901ae946
Heidi Chan <z5024971@student.unsw.edu.au>
2021-08-28 12:00:19 +1000

    Basic styling for nav between filter/snapshot

--- src/popup.tsx
+++ src/popup.tsx
@@ -2,51 +2,33 @@
 
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import Button from "@atlaskit/button";
+import CameraIcon from '@atlaskit/icon/glyph/camera';
+import WatchIcon from '@atlaskit/icon/glyph/watch';
 
 const Popup = () => {
-  const [count, setCount] = useState(0);
   const [currentURL, setCurrentURL] = useState<string>();
+  const [page, setPage] = useState(0);
 
-  useEffect(() => {
-    chrome.browserAction.setBadgeText({ text: count.toString() });
-  }, [count]);
-
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      setCurrentURL(tabs[0].url);
-    });
-  }, []);
-
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const tab = tabs[0];
-      if (tab.id) {
-        chrome.tabs.sendMessage(
-          tab.id,
-          {
-            color: "#555555",
-          },
-          (msg) => {
-            console.log("result message:", msg);
-          }
-        );
-      }
-    });
-  };
 
   return (
     <>
-      <ul style={{ minWidth: "700px" }}>
-        <li>Current URL: {currentURL}</li>
-        <li>Current Time: {new Date().toLocaleTimeString()}</li>
-      </ul>
-      <button
-        onClick={() => setCount(count + 1)}
-        style={{ marginRight: "5px" }}
-      >
-        count up
-      </button>
-      <button onClick={changeBackground}>change background</button>
+      <div className="extension-container">
+        {page ? (
+          <h1>Filter stuff</h1>
+        ) : (
+          <h1>Snapshot stuff</h1>
+        )}
+      </div>
+      <div className="buttons">
+        <Button iconBefore={<CameraIcon label="Camera icon" size="small"/>} onClick={() => setPage(0)} className="info-button">
+          Snapshot
+        </Button>
+        <Button iconBefore={<WatchIcon label="Watch icon" size="small"/>} onClick={() => setPage(1)} className="snapshot-button">
+          Filter
+        </Button>
+
+      </div>
     </>
   );
 };
