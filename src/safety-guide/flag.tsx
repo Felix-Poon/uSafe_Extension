9f68f18d61066f5a3bd83d03f9a637647d5e9af5
Declan Warn <dwarn@atlassian.com>
2021-08-28 21:20:04 +1000

    Use shadow root for flag to protect styles

--- src/safety-guide/flag.tsx
+++ src/safety-guide/flag.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 
 import InfoIcon from "@atlaskit/icon/glyph/info";
 import { B300 } from "@atlaskit/theme/colors";
@@ -12,57 +12,80 @@ import {
   easeIn,
 } from "@atlaskit/motion";
 
+import root from "react-shadow";
+
+import { CacheProvider } from "@emotion/core";
+import createCache from "@emotion/cache";
+
 type SafetyGuideFlagProps = {
   href: string;
   name: string;
 };
 
 const SafetyGuideFlag = ({ href, name }: SafetyGuideFlagProps) => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = React.useState(true);
+  const [cacheContainer, setCacheContainer] =
+    React.useState<HTMLElement | null>(null);
 
-  const hideFlag = useCallback(() => {
+  const hideFlag = React.useCallback(() => {
     setShow(false);
   }, [setShow]);
 
+  const myCache = React.useMemo(() => {
+    if (cacheContainer) {
+      return createCache({
+        key: "content-script",
+        container: cacheContainer,
+      });
+    }
+  }, [cacheContainer]);
+
   return (
-    <div
-      style={{
-        position: "fixed",
-        maxWidth: "400px",
-        zIndex: 9999,
-        right: 16,
-        top: 8,
-      }}
-    >
-      <ExitingPersistence appear>
-        {show && (
-          <SlideIn
-            enterFrom="right"
-            fade="in"
-            duration={mediumDurationMs}
-            animationTimingFunction={() => easeIn}
-          >
-            {(props) => (
-              <div {...props}>
-                <Flag
-                  icon={<InfoIcon label="Info" primaryColor={B300} />}
-                  id="info"
-                  key="info"
-                  title="E-Safety Guide Available!"
-                  description={`Learn how to protect yourself and report inappropriate content while browsing ${name}.`}
-                  actions={[
-                    // View guide opens in new tab
-                    { content: "View Guide", href },
-                    { content: "Close", onClick: hideFlag },
-                  ]}
-                  onDismissed={hideFlag}
-                />
-              </div>
-            )}
-          </SlideIn>
+    <root.div>
+      <div
+        ref={setCacheContainer}
+        style={{
+          position: "fixed",
+          maxWidth: "400px",
+          zIndex: 9999,
+          right: 16,
+          top: 8,
+        }}
+      >
+        {myCache && (
+          <CacheProvider value={myCache}>
+            <ExitingPersistence appear>
+              {show && (
+                <SlideIn
+                  enterFrom="right"
+                  fade="in"
+                  duration={mediumDurationMs}
+                  animationTimingFunction={() => easeIn}
+                >
+                  {(props) => (
+                    <div {...props}>
+                      <Flag
+                        icon={<InfoIcon label="Info" primaryColor={B300} />}
+                        id="info"
+                        key="info"
+                        title="E-Safety Guide Available!"
+                        description={`Learn how to protect yourself and report inappropriate content while browsing ${name}.`}
+                        actions={[
+                          // View guide opens in new tab
+                          { content: "View Guide", href },
+                          { content: "Close", onClick: hideFlag },
+                        ]}
+                        onDismissed={hideFlag}
+                      />
+                    </div>
+                  )}
+                </SlideIn>
+              )}
+            </ExitingPersistence>
+          </CacheProvider>
         )}
-      </ExitingPersistence>
-    </div>
+      </div>
+    </root.div>
   );
 };
 
